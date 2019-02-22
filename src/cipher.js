4836f221970af665660b04007ebe550737883e1a
melilove <melissa.salazar13@yahoo.com>
2019-02-22 09:12:05 -0500

    hacer cambios en html

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,17 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset,string) => {
+    for (let i = 0; i < string.length; i++) {
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90) {
+        let numberAscii = ((string[i].charCodeAt())-65+offset)%26+65;
+        let lettercipher = String.fromCharCode(numberAscii);
+        newMessage = newMessage + lettercipher;
+      } else {
+      newMessage = newMessage+string[i];
+      }
+    }
+    return newMessage;
   },
+
   decode: () => {
     /* Acá va tu código */
   }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-/* Acá va tu código */
+// Para el html contacto //
+function obtenerdatos() {
+    let comment =document.getElementById('comment').value;
+    let author = document.getElementById('author').value;
+    let email = document.getElementById('email').value;
+    let submit = document.getElementById('submit').value;
+    
+    document.registro.commentObtenido.value = comment;
+    document.registro.authorObtenido.value = author;
+    document.registro.emailObtenido.value = email;
+    document.registro.submitObtenido.value = submit;
+};
+// Ahora haciendo la funcion cifrar //
+
+const btnCipher = document.getElementById('submit');
+const message = document.getElementById('commentCifrar');
+const codDes = document.getElementById('contraseñaCifrar');
+const message1 = document.getElementById('commentCifrar');
+
+function hacerClickCifrar() {
+    let mensaje = message.value;
+    let numero = parseInt(codDes.value);
+    let newMessage = cipher.encode(numero,mensaje);
+return message1.value = newMessage;
+}
+
+btnCipher.addEventListener("click",hacerClickCifrar)
