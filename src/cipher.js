dc60e3aebfd95677d9f1cf720af72da1910687d3
melilove <melissa.salazar13@yahoo.com>
2019-02-26 17:04:59 -0500

    creando la funcion del cifrado y descifrado de las minusculas y los numeros

--- src/cipher.js
+++ src/cipher.js
@@ -2,16 +2,22 @@ window.cipher = {
   encode: (offset,string) => {
 
     let newMessage ='';
-    string = string.toUpperCase(); // es para volver mayuscula //
     for (let i = 0; i < string.length; i++) {
-      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90) {
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90) { 
         let numberAscii = ((string[i].charCodeAt())-65+offset)%26+65;
         let lettercipher = String.fromCharCode(numberAscii);
-        lettercipher = lettercipher.toLowerCase();// es para volver minuscula //
-        newMessage = newMessage + lettercipher;
-      } else {
-      newMessage = newMessage+string[i];
-      }
+        newMessage = newMessage + lettercipher; // LETRAS MAYUSCULAS //
+      }else if(string[i].charCodeAt()>=97 && string[i].charCodeAt()<=122) {
+        let numberAscii = ((string[i].charCodeAt())-97+offset)%26+97;
+        let lettercipher = String.fromCharCode(numberAscii);
+        newMessage = newMessage + lettercipher; // LETRAS MINUSCULAS //
+      }else if(string[i].charCodeAt()>=48 && string[i].charCodeAt()<=57) {
+        let numberAscii = ((string[i].charCodeAt())-48+offset)%10+48;
+        let lettercipher = String.fromCharCode(numberAscii);
+        newMessage = newMessage + lettercipher; // NUMEROS //
+    } else {
+      newMessage = newMessage+string[i];  
+    }
     }
     return newMessage;
   },
@@ -19,13 +25,19 @@ window.cipher = {
   decode: (offset,string) => {
 
     let newMessage ='';
-    string = string.toUpperCase();
     for (let i = 0; i < string.length; i++) {
       if(string[i].charCodeAt() >= 65 && string[i].charCodeAt() <=90) {
         let numberAscii = ((string[i].charCodeAt())-90-offset)%26+90;
         let lettercipher = String.fromCharCode(numberAscii);
-        lettercipher = lettercipher.toLowerCase();
-        newMessage = newMessage + lettercipher;
+        newMessage = newMessage + lettercipher; // LETRAS MAYUSCULAS //
+      }else if(string[i].charCodeAt()>=97 && string[i].charCodeAt()<=122) {
+        let numberAscii = ((string[i].charCodeAt())-122-offset)%26+122;
+        let lettercipher = String.fromCharCode(numberAscii);
+        newMessage = newMessage + lettercipher; // LETRAS MINUSCULAS //
+      }else if(string[i].charCodeAt()>=48 && string[i].charCodeAt()<=57) {
+        let numberAscii = ((string[i].charCodeAt())-57-offset)%10+57;
+        let lettercipher = String.fromCharCode(numberAscii);
+        newMessage = newMessage + lettercipher; // NUMEROS //
       } else {
        newMessage = newMessage + string[i];
       }
@@ -33,3 +45,4 @@ window.cipher = {
     return newMessage;
 }
 }
+
