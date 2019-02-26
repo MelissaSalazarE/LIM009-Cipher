e3107bfbe1b42ac1dac6f23d5701839f51e89c03
melilove <melissa.salazar13@yahoo.com>
2019-02-26 11:16:15 -0500

    creando rama

--- src/cipher.js
+++ src/cipher.js
@@ -2,11 +2,12 @@ window.cipher = {
   encode: (offset,string) => {
 
     let newMessage ='';
-
+    string = string.toUpperCase(); // es para volver mayuscula //
     for (let i = 0; i < string.length; i++) {
       if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90) {
         let numberAscii = ((string[i].charCodeAt())-65+offset)%26+65;
         let lettercipher = String.fromCharCode(numberAscii);
+        lettercipher = lettercipher.toLowerCase();// es para volver minuscula //
         newMessage = newMessage + lettercipher;
       } else {
       newMessage = newMessage+string[i];
@@ -18,11 +19,12 @@ window.cipher = {
   decode: (offset,string) => {
 
     let newMessage ='';
-
+    string = string.toUpperCase();
     for (let i = 0; i < string.length; i++) {
       if(string[i].charCodeAt() >= 65 && string[i].charCodeAt() <=90) {
         let numberAscii = ((string[i].charCodeAt())-90-offset)%26+90;
         let lettercipher = String.fromCharCode(numberAscii);
+        lettercipher = lettercipher.toLowerCase();
         newMessage = newMessage + lettercipher;
       } else {
        newMessage = newMessage + string[i];
