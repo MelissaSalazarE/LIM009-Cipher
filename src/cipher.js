f674eeb18df4a0eb74427b32ca3a8d0f8df6a0cf
melilove <melissa.salazar13@yahoo.com>
2019-02-28 23:01:43 -0500

    LOS TEST AL 100%

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,13 @@
 window.cipher = {
   encode: (offset,string) => {
-     offset = offset%65 ;
     let newMessage ='';
     for (let i = 0; i < string.length; i++) {
       if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90) { 
-        let numberAscii = ((string[i].charCodeAt())-65+offset)%26+65;
+        let numberAscii = (((string[i].charCodeAt())-65+offset)%26+26)%26+65;
         let lettercipher = String.fromCharCode(numberAscii);
         newMessage = newMessage + lettercipher; // LETRAS MAYUSCULAS //
       }else if(string[i].charCodeAt()>=97 && string[i].charCodeAt()<=122) {
-        let numberAscii = ((string[i].charCodeAt())-97+offset)%26+97;
+        let numberAscii = (((string[i].charCodeAt())-97+offset)%26+26)%26+97;
         let lettercipher = String.fromCharCode(numberAscii);
         newMessage = newMessage + lettercipher; // LETRAS MINUSCULAS //
       }else if(string[i].charCodeAt()>=48 && string[i].charCodeAt()<=57) {
@@ -24,15 +23,14 @@ window.cipher = {
 
   decode: (offset,string) => {
     
-    offset = offset%65 ;
     let newMessage ='';
     for (let i = 0; i < string.length; i++) {
       if(string[i].charCodeAt() >= 65 && string[i].charCodeAt() <=90) {
-        let numberAscii = ((string[i].charCodeAt())-90-offset)%26+90;
+        let numberAscii = (((string[i].charCodeAt())-90-offset)%26-26)%26+90;
         let lettercipher = String.fromCharCode(numberAscii);
         newMessage = newMessage + lettercipher; // LETRAS MAYUSCULAS //
       }else if(string[i].charCodeAt()>=97 && string[i].charCodeAt()<=122) {
-        let numberAscii = ((string[i].charCodeAt())-122-offset)%26+122;
+        let numberAscii = (((string[i].charCodeAt())-122-offset)%26-26)%26+122;
         let lettercipher = String.fromCharCode(numberAscii);
         newMessage = newMessage + lettercipher; // LETRAS MINUSCULAS //
       }else if(string[i].charCodeAt()>=48 && string[i].charCodeAt()<=57) {
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -15,7 +15,20 @@ describe('cipher', () => {
     it('deberia retornar " " para " " con offest 33');
     assert.equal(cipher.encode(33," ")," ");
     });
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33',()=>{
+      assert.equal(cipher.encode(33,"abcdefghijklmnopqrstuvwxyz"),"hijklmnopqrstuvwxyzabcdefg");
+    it('deberia retornar " " para " " con offest 33');
+    assert.equal(cipher.encode(33," ")," ");
+    });
+
+    it('debería retornar "456789012" para "123456789" con offset 33',()=>{
+      assert.equal(cipher.encode(33,"123456789"),"456789012");
+    it('deberia retornar " " para " " con offest 33');
+    assert.equal(cipher.encode(33," ")," ");
+    });
   });
+
+
 
   describe('cipher.decode', () => {
 
@@ -24,8 +37,19 @@ describe('cipher', () => {
     });
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',()=>{
-    assert.equal(cipher.encode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     it('deberia retornar " " para " " con offest 33');
     assert.equal(cipher.decode(33," ")," ");
+    });
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset 33',()=>{
+      assert.equal(cipher.decode(33,"hijklmnopqrstuvwxyzabcdefg"),"abcdefghijklmnopqrstuvwxyz");
+    it('deberia retornar " " para " " con offest 33');
+      assert.equal(cipher.decode(33," ")," ");
+    });
+    it('debería retornar "123456789" para "456789012" con offset 33',()=>{
+      assert.equal(cipher.decode(33,"456789012"),"123456789");
+   it('deberia retornar " " para " " con offest 33');
+      assert.equal(cipher.decode(33," ")," ");
+    });
   });
-});
+})
