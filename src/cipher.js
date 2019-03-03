c918eb4ac4c25d156a39913e25328b2491db681f
melilove <melissa.salazar13@yahoo.com>
2019-03-03 16:40:49 -0500

    Terminando el readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,22 @@
 window.cipher = {
   encode: (offset, string) => {
     let newMessage = '';
-    
+
     for (let i = 0; i < string.length; i++) {
       let a;
       let b;
-      if (string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90) {
+      if (string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90) { // rango de las letras Mayusculas //
         a = 65;
         b = 26;
-      } else if (string[i].charCodeAt() >= 97 && string[i].charCodeAt() <= 122) {
+      } else if (string[i].charCodeAt() >= 97 && string[i].charCodeAt() <= 122) { // rango de las letras Minuscula //
         a = 97;
         b = 26;
-      } else if (string[i].charCodeAt() >= 48 && string[i].charCodeAt() <= 57) {
+      } else if (string[i].charCodeAt() >= 48 && string[i].charCodeAt() <= 57) { // rango de los numeros //
         a = 48;
         b = 10;
-      } else if (string[i].charCodeAt() >= 140 && string[i].charCodeAt() <= 165) {
-        a = 140;
-        b = 26;
+      } else if (string[i].charCodeAt() >= 209 && string[i].charCodeAt() <= 252) { // rango para hallar la ñ //
+        a = 209;
+        b = 43;
       } else {
         newMessage = newMessage + string[i];
         continue;
@@ -25,9 +25,40 @@ window.cipher = {
       let lettercipher = String.fromCharCode(numberAscii);
       newMessage = newMessage + lettercipher;
     }
-    return newMessage; 
+    return newMessage;
+  },
+
+decode: (offset, string) => {
+    
+  let newMessage = '';
+
+    for (let i = 0; i < string.length; i++) {
+      let a;
+      let b;
+      if (string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90) {
+        a = 90;
+        b = 26;
+      } else if (string[i].charCodeAt() >= 97 && string[i].charCodeAt() <= 122) {
+        a = 122;
+        b = 26;
+      } else if (string[i].charCodeAt() >= 48 && string[i].charCodeAt() <= 57) {
+        a = 57;
+        b = 10;
+      } else if (string[i].charCodeAt() >= 209 && string[i].charCodeAt() <= 252) {
+        a = 252;
+        b = 43;
+      } else {
+        newMessage = newMessage + string[i];
+        continue;
+      }
+      const numberAscii = (((string[i].charCodeAt()) - a - offset) % b - b) % b + a;
+      let lettercipher = String.fromCharCode(numberAscii);
+      newMessage = newMessage + lettercipher;
+    }
+    return newMessage;
   }
 };
+
 
 
 // window.cipher = {
