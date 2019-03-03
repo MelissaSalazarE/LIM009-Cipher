969e27fbb8a5f964e7f3b736c79f761b7613e851
melilove <melissa.salazar13@yahoo.com>
2019-03-03 18:44:32 -0500

    Consolidando los 4 html en 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
+// Capturando los html //
+
+
+const seccionInicio = document.getElementById('seccion-inicio');
+const seccionServicios = document.getElementById('seccion-servicios');
+const seccionContacto = document.getElementById('seccion-contacto');
+const seccionTestimonios = document.getElementById('seccion-testimonios');
+
+const paginaInicio = document.getElementById('pagina-inicio');
+const paginaServicio = document.getElementById('pagina-servicios');
+const paginaContacto = document.getElementById('pagina-contacto');
+const paginaTestimonio = document.getElementById('pagina-testimonios');
+
+paginaInicio.style.display="block";
+paginaServicio.style.display="none";
+paginaContacto.style.display="none";
+paginaTestimonio.style.display="none";
+
+
+seccionInicio.addEventListener('click',() => {
+    paginaInicio.style.display="block";
+    paginaServicio.style.display="none";
+    paginaContacto.style.display="none";
+    paginaTestimonio.style.display="none"; 
+});
+
+seccionServicios.addEventListener('click',() => {
+    paginaInicio.style.display="none";
+    paginaServicio.style.display="block";
+    paginaContacto.style.display="none";
+    pginaTestimonio.style.display="none"; 
+});
+seccionContacto.addEventListener('click',() => {
+    paginaInicio.style.display="none";
+    paginaServicio.style.display="none";
+    paginaContacto.style.display="block";
+    paginaTestimonio.style.display="none"; 
+});
+seccionTestimonios.addEventListener('click',() => {
+    paginaInicio.style.display="none";
+    paginaServicio.style.display="none";
+    paginaContacto.style.display="none";
+    paginaTestimonio.style.display="block"; 
+});
+
+
+
+
 // Ahora haciendo la funcion cifrar //
 
 const btnCipher = document.getElementById('btn-cifrar');
@@ -45,3 +93,4 @@ function obtenerdatos() {
     document.registro.submitObtenido.value = submit;
 }
 /* eslint-enable no-unused-vars */
+
